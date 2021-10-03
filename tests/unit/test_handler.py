4c9eda7e388210f501a866411aff268de7f2b17c
masaue <masayuki.uegaki@gmail.com>
2021-10-03 22:46:09 +0900

    fix format by black

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -7,7 +7,7 @@ from app import app
 
 @pytest.fixture()
 def apigw_event():
-    """ Generates API GW Event"""
+    """Generates API GW Event"""
 
     return {
         "body": '{ "test": "body"}',
