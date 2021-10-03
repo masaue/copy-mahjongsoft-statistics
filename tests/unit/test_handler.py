e3d624bb93c0b165c12ecf1e2a43e892a7e65dc0
masaue <masayuki.uegaki@gmail.com>
2021-10-03 22:53:07 +0900

    fix "E501 line too long"

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -36,19 +36,22 @@ def apigw_event():
         },
         "queryStringParameters": {"foo": "bar"},
         "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Via": ("1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net "
+                    "(CloudFront)"),
             "Accept-Language": "en-US,en;q=0.8",
             "CloudFront-Is-Desktop-Viewer": "true",
             "CloudFront-Is-SmartTV-Viewer": "false",
             "CloudFront-Is-Mobile-Viewer": "false",
             "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
             "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept": ("text/html,application/xhtml+xml,application/xml;"
+                       "q=0.9,image/webp,*/*;q=0.8"),
             "Upgrade-Insecure-Requests": "1",
             "X-Forwarded-Port": "443",
             "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
             "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "X-Amz-Cf-Id": ("aaaaaaaaaae3VYQb9jd-nvCd-"
+                            "de396Uhbp027Y2JvkCPNLmGJHqlaA=="),
             "CloudFront-Is-Tablet-Viewer": "false",
             "Cache-Control": "max-age=0",
             "User-Agent": "Custom User Agent String",
